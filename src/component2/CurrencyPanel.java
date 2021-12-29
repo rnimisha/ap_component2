67d7c2f26d561a22eb6e4b966bd744a49705cd56
rnimisha <nimisaraut.gmail.com>
2021-12-29 20:30:30 +0545

    action listeners added

--- src/component2/CurrencyPanel.java
+++ src/component2/CurrencyPanel.java
@@ -1,8 +1,11 @@
 package component2;
 //importing java packages to be used in the program
 import javax.swing.*;
+
+
 import java.awt.*;
 import java.awt.event.*;
+import java.text.DecimalFormat;
 
 //inherits built-in JFrame class
 public class CurrencyPanel extends JPanel{
@@ -19,7 +22,7 @@ public class CurrencyPanel extends JPanel{
 	/*Button that will cause conversion action to be performed when clicked*/
 	private JButton convertButton;
 	private JLabel resultValue; //for converted result
-	private JTextField inputData; //area for user input
+	private JTextField enteredAmount; //area for user input
 	/*additonal button to clear back to start look*/
 	private JButton resetButton;
 	/*additional label displaying counting total number of conversions performed */
@@ -28,6 +31,8 @@ public class CurrencyPanel extends JPanel{
 	private int conversionCount=0;
 	/*additional checkbox for switching between reverse and normal conversion*/
 	private JCheckBox reverseBox;
+	/*will be set true if reverse option is selected*/
+	private boolean checked=false;
 	
 	
 	
@@ -55,13 +60,15 @@ public class CurrencyPanel extends JPanel{
 		
 		/*JTextField with 5 columns 
 		 * user input value in this field*/
-		inputData = new JTextField(5);
-		inputData.setToolTipText("Enter amount that needs to be converted here" );
+		enteredAmount = new JTextField(5);
+		enteredAmount.setToolTipText("Enter amount that needs to be converted here" );
 		
 		
 		//additional button to clear
 		resetButton=new JButton ("Reset");
 		resetButton.setToolTipText("press to reset all datas");
+		//Button will initially be disabled
+		resetButton.setEnabled(false);
 		
 		//setting additional label for counting
 		countLabel=new JLabel("Conversion count : "+conversionCount );
@@ -70,6 +77,23 @@ public class CurrencyPanel extends JPanel{
 		//checkbox to reverse conversion
 		reverseBox=new JCheckBox("reverse conversion");
 		reverseBox.setToolTipText("Converesion done from selected unit to pounds when selected.");
+		
+		/*-------Adding ActionListener to components----*/
+		
+		//for converting amount and incrementing conversionCount 
+		ConvertListener convertAmount=new ConvertListener();
+		//to call actionlistener when button is pressed
+		convertButton.addActionListener(convertAmount); 
+		//to call actionlistener when enter is pressed on textfield
+		enteredAmount.addActionListener(convertAmount); 
+		
+		//for reverse conversion
+		//itemListener as it is checkbox
+		reverseBox.addItemListener(new ReverseListener());
+		
+		//for resetting
+		resetButton.addActionListener(new ResetListener());
+		
 		
 		
 		/*----- Changing Layout -------*/
@@ -101,7 +125,7 @@ public class CurrencyPanel extends JPanel{
 		//add components in order to be displayed in innerPaneel
 		innerPanel.add(currencyComboBox, BorderLayout.NORTH);
 		innerPanel.add(inputLabel, BorderLayout.WEST);
-		innerPanel.add(inputData, BorderLayout.CENTER);
+		innerPanel.add(enteredAmount, BorderLayout.CENTER);
 		innerPanel.add(resultValue, BorderLayout.EAST);
 		//add another panel in south
 		innerPanel.add(innerPanel2, BorderLayout.SOUTH);
@@ -115,8 +139,6 @@ public class CurrencyPanel extends JPanel{
 		setPreferredSize(new Dimension(500, 200));
 		setBackground(new Color(137, 181, 217));
 	}
-	
-	
 	
 	
 	//method defination to design and return menu
@@ -203,6 +225,177 @@ public class CurrencyPanel extends JPanel{
 		
 		//returns final menu designed
 		return menuBar;
+		
+	}
+
+	/* defination for ConvertListener
+	 * converts amount entered by user to pounds or pounds to selected currency 
+	 * and sets result in two decimal place
+	 * also increments total conversions performed
+	 * will be implementing ActionListener interface */
+	public class ConvertListener implements ActionListener
+	{
+
+		@Override
+		//overriding method as per needed
+		//when actionlistener catches an event
+		public void actionPerformed(ActionEvent event) 
+		{
+			
+			/*storing after removing white space in front and back from currency entered by user*/
+			String amount=enteredAmount.getText().trim();
+			
+			//check if value entered is null by user
+			if(!(amount.isEmpty()))
+			{
+				//exception handling
+				try {
+					//convert String to Double 
+					double numericAmount = Double.parseDouble(amount);
+					
+					double factor=0; //conversion factor
+					String symbol=""; //Symbol of currency
+					double result=0; //final result
+					//check which option is selected according to index
+					switch(currencyComboBox.getSelectedIndex())
+					{
+						case 0: //Japanese Yen
+							factor=137.52;
+							symbol="¥";
+							break; //end switch
+						case 1: //Euro
+							factor=1.09;
+							symbol="€";
+							break; //end switch
+						case 2: //US Dollar
+							factor=1.29;
+							symbol="$";
+							break; //end switch
+						case 3: //Australian Dollars
+							factor=1.78;
+							symbol="A$";
+							break; //end switch
+						case 4: //Canadian Dollars
+							factor=1.70;
+							symbol="C$";
+							break; //end switch
+						case 5: //South Korean Won
+							factor=1537.75;
+							symbol="₩";
+							break; //end switch
+						case 6: //Thai Bhat
+							factor=40.52;
+							symbol="฿";
+							break; //end switch
+						case 7: //United Arab Emirates Dirham
+							factor=4.75;
+							symbol="د.إ";
+							break; //end switch	
+					}
+					
+					//change decimal places to two
+					DecimalFormat df = new DecimalFormat("0.00");
+					
+					String answer;
+					//in case reverse button is not selected
+					if(!checked)
+					{
+						result=numericAmount*factor;
+						//df.format returns string
+						answer=symbol+" "+df.format(result);
+					}
+					else {
+						result=numericAmount/factor;
+						//df.format returns string
+						answer="£ "+df.format(result);
+					}
+					
+					//change label holding result to converted answer
+					resultValue.setText(answer);
+					
+					//enable clearButton
+					resetButton.setEnabled(true);	
+					
+					//increment conversion count
+					conversionCount++;
+					//change conversion count label text with updated value
+					countLabel.setText("Conversion count : "+conversionCount );
+					
+				}
+				//if format of amount is not numeric then
+				catch(NumberFormatException e)
+				{
+					//pop message dialogue box to inform about incorrect data format error
+					JOptionPane.showMessageDialog(new JFrame(),"Please enter data in correct numeric format."," Data format not matched!",JOptionPane.ERROR_MESSAGE);
+				}
+				
+			}
+			/*if nothing is entered in the textfield
+			 * will display dialogue box with message*/
+			else 
+			{
+				//JOptionPane class used to pop up a message dialogue box
+				JOptionPane.showMessageDialog(new JFrame(),"Please enter value on textfield."," Data Missing!",JOptionPane.ERROR_MESSAGE);
+				
+			}
+
+		}
+	}
+	
+	
+	//listener to reverse units that implements ItemListener
+	//when clicked on checkbox
+	public class ReverseListener implements ItemListener
+	{
+		//overriding method as per needed
+		//invoked when click or unclicked on checkbox
+		public void itemStateChanged(ItemEvent event)
+		{
+			//if user checks checkbox
+			/*checked is used to perform calculation
+			 * if true then normal conversion from pounds will happen
+			 * if false then other currency will be converted to pounds*/
+			if(reverseBox.isSelected())
+			{
+				checked=true; //reverse checkbox is checked
+			}
+			else
+			{
+				checked=false;//reverse checkbox is not checked
+			}
+		}
+	}
+	
+	/*actionListener for resetting
+	 * will change all panel component everyting back to default*/
+	public class ResetListener implements ActionListener
+	{
+
+		@Override 
+		//overriding to make it perform actions as required 
+		public void actionPerformed(ActionEvent e) {
+			
+			///clears input box
+			enteredAmount.setText("");
+			
+			//clears result label
+			resultValue.setText("---");
+			
+			//set count back to zero
+			conversionCount=0;
+			//show changes
+			countLabel.setText("Conversion count : "+conversionCount );
+			
+			//disable clear button
+			resetButton.setEnabled(false);
+			
+			//to uncheck reverse as well
+			reverseBox.setSelected(false);
+			
+			//change combo box option back to default
+			currencyComboBox.setSelectedIndex(0);
+			
+		}
 		
 	}
 
